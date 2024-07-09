dca6a4c44d38d0777d6b06feb1345a951dd19971
RenzoFantu <rfantuzzi@gmail.com>
2024-07-09 14:44:10 -0400

    add cors to index.js

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors')
 const routes = require('./src/routes/index');
 const app = express();
 
 const port = process.env.PORT || 3000;
 
-
+app.use(cors())
 app.use(express.json());
 
 mongoose.connect(process.env.MONGODB_URI);
